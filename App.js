5eabdbaf0a71e0793a4a892e99ecb4b2a09a79e3
AdarshMitr <adarshkumar019@gmail.com>
2023-08-01 12:39:00 +0530

    creating body for FoodApp

--- App.js
+++ App.js
@@ -5,25 +5,57 @@ import Logo from "./Logo.png";
 // Creating Food Ordering App
 
 const Header = () => {
-  return <div className="header">
-    <div id="logo-container">
-      <img id="logo" src={Logo}/>
-    <div className="nav-items">
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Cart</li>
-      </ul>
+  return (
+    <div div className="header">
+      <div className="logo-container">
+        <img id="logo" src={Logo} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-  </div>
-  </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{backgroundColor:'white'}}>
+      <img className="res-logo"  alt="res-logo" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/08eee882fda81b68e5c36e3a5d89d8c0"/>
+      <h3>McDonald's</h3>
+      <h4>American, Fast Food</h4>
+      <h4>4.0 ⭐⭐⭐⭐</h4>
+      <h4>15 minutes</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard />
+      </div>
+    </div>
+  );
 };
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };

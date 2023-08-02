ed5c0af3c79ff2b96aec471280dbbbb5f3e9b38e
AdarshMitr <adarshkumar019@gmail.com>
2023-08-02 12:04:12 +0530

    passing props to cards

--- App.js
+++ App.js
@@ -22,13 +22,15 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+
+ const {resName,cuisine,ratings}= props;
   return (
     <div className="res-card" style={{backgroundColor:'white'}}>
       <img className="res-logo"  alt="res-logo" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/08eee882fda81b68e5c36e3a5d89d8c0"/>
-      <h3>McDonald's</h3>
-      <h4>American, Fast Food</h4>
-      <h4>4.0 ⭐⭐⭐⭐</h4>
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
+      <h4>{ratings}</h4>
       <h4>15 minutes</h4>
     </div>
   );
@@ -38,17 +40,13 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard />
+        <RestaurantCard resName="McDonald's" cuisine="Burger,Fast Food" ratings="3.0 ⭐⭐⭐"/>
+        <RestaurantCard resName="Royal Rasoi" cuisine="Kadhai Paneer,Boondi Rayta" ratings="4.0 ⭐⭐⭐⭐" />
+         
       </div>
     </div>
   );
+  
 };
 
 const AppLayout = () => {

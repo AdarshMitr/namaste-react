1d2ad0b9b47c2d4c12beb2ecb3f05b91ef78cb6c
AdarshMitr <adarshkumar019@gmail.com>
2023-08-08 10:21:00 +0530

    added search functionality

--- components/Body.js
+++ components/Body.js
@@ -1,54 +1,85 @@
-import React  from "react";
+import React from "react";
 import RestaurantCard from "./RestaurantCard";
-import { useState, useEffect} from "react";
-import Shimmer from "./Shimmer"; 
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-
   // Local State Variable-super powerful variable
-  const [listOfRestaurants,setListOfRestaurants] = useState([]);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  console.log('Body Renders'); 
 
-  useEffect(()=>{
-   fetchData();
-  },[]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-const fetchData=async()=>{
-  const data=await fetch(
-    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4594965&lng=77.0266383&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-  );
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4594965&lng=77.0266383&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
-  const json=await data.json();
-  console.log(json);
+    const json = await data.json();
+    console.log(json);
 
-  //Optional Chaining
+    //Optional Chaining
 
-  setListOfRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-};
-
-
-
-
-    return listOfRestaurants?.length === 0 ? <Shimmer/>:(
-      <div className="body">
-        <div className="filter">
-          <button className="filter-btn" onClick={()=>{const filteredList=listOfRestaurants.filter((res)=>res.info.avgRating > 4)
-          setListOfRestaurants(filteredList);
-          }}>Top Rated Restaurants</button>
-        </div>  
-        <div className="res-container">
-  
-          {/* using map to iterate over resList elements */}
-  
-         { listOfRestaurants?.map((restaurant)=>(
-  <RestaurantCard key={restaurant.info.id}resData={restaurant}/>
-          ))
-          }
-       
-          
-          
-        </div>
-      </div>
+    setListOfRestaurants(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
 
-  export default Body;
+  return listOfRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+              console.log(searchText)
+            }}
+          />
+
+          <button
+            onClick={() => {
+              {
+                /* Filter the Restaurant cards and update the UI */
+              }
+              console.log(searchText);
+              const filteredRestaurant=listOfRestaurants.filter((res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase()))
+              setListOfRestaurants(filteredRestaurant);
+            }}
+            
+          >
+            Search
+          </button>
+        </div>
+
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {/* using map to iterate over resList elements */}
+
+        {listOfRestaurants?.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;

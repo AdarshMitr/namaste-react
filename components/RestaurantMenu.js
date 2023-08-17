8ca382a8ad153f59752a11c40d8ccad16061ca3b
AdarshMitr <adarshkumar019@gmail.com>
2023-08-17 13:19:50 +0530

    api updated

--- components/RestaurantMenu.js
+++ components/RestaurantMenu.js
@@ -6,24 +6,25 @@ const RestaurantMenu = () => {
     fetchMenu();
   }, []);
 
- const fetchMenu = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4594965&lng=77.0266383&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING&restaurantId=${94736}");
-    
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4594965&lng=77.0266383&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
     const json = await data.json();
     console.log(json);
     setResInfo(json.data);
   };
 
-  // const { name, cuisines, costForTwoMessage } =
-  //   resInfo?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0]?.info;
+  if (resInfo == null) return <Shimmer />;
 
-  return resInfo == null ? (
-    <Shimmer />
-  ) : (
+  const { name, cuisines, costForTwo } =
+  resInfo?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants[2]?.info;
+  return (
     <div>
       <h1>{name}</h1>
-      <h3>{cuisines}</h3>
-      <h3>{costForTwoMessage}</h3>
+      <h3>{cuisines.toString()}</h3>
+      <h3>{costForTwo}</h3>
       <ul>
         <li>Chole Bhature</li>
         <li>Paneer Pakode</li>

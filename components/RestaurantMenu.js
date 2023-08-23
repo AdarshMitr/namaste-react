301d28793b38f982ec3a12b20c9246ce609a5ce2
AdarshMitr <adarshkumar019@gmail.com>
2023-08-23 13:42:18 +0530

    make resId dynamic by using 'useParams'

--- components/RestaurantMenu.js
+++ components/RestaurantMenu.js
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { MENU_API } from "../utils/constants";
 const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState(null);
+  const {resId}=useParams();
+  console.log(resId);
   useEffect(() => {
     fetchMenu();
   }, []);
 
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7272832&lng=77.3370089&restaurantId=56478&catalog_qa=undefined&submitAction=ENTER"
+      MENU_API+resId
     );
 
     const json = await data.json();
@@ -18,21 +22,27 @@ const RestaurantMenu = () => {
 
   if (resInfo === null) return <Shimmer />;
   const { name, cuisines, costForTwoMessage } =
-  resInfo?.cards[0]?.card?.card?.info;
-  const {itemCards}=resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+    resInfo?.cards[0]?.card?.card?.info;
+  const { itemCards } =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
   console.log(itemCards);
 
   return (
     <div>
       <h1>{name}</h1>
-      <h3>{cuisines.join(",")} - {costForTwoMessage} </h3>
+      <h3>
+        {cuisines.join(",")} - {costForTwoMessage}{" "}
+      </h3>
 
       <h2>Menu</h2>
 
       <ul>
-       
-      {itemCards.map(item=><li key={item.card.info.id}>{item.card.info.name} - Rs.{item.card.info.price/100||item.card.info.defaultPrice/100}</li>)}
-        
+        {itemCards.map((item) => (
+          <li key={item.card.info.id}>
+            {item.card.info.name} - Rs.
+            {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+          </li>
+        ))}
       </ul>
     </div>
   );
--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
 
 export const CDN_URL= "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-
+export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7272832&lng=77.3370089&restaurantId=";

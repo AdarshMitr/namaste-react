d4b33744028e88344e700c40ffbcfafa6c5c7f1d
AdarshMitr <adarshkumar019@gmail.com>
2023-08-23 12:53:02 +0530

    mapped single restaurantMenu items

--- components/RestaurantMenu.js
+++ components/RestaurantMenu.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
-import { Link } from 'react-router-dom';
 const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState(null);
   useEffect(() => {
@@ -9,7 +8,7 @@ const RestaurantMenu = () => {
 
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7272832&lng=77.3370089&restaurantId=32128&catalog_qa=undefined&submitAction=ENTER"
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7272832&lng=77.3370089&restaurantId=56478&catalog_qa=undefined&submitAction=ENTER"
     );
 
     const json = await data.json();
@@ -18,19 +17,21 @@ const RestaurantMenu = () => {
   };
 
   if (resInfo === null) return <Shimmer />;
-
   const { name, cuisines, costForTwoMessage } =
   resInfo?.cards[0]?.card?.card?.info;
-  const {itemCards}=resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+  const {itemCards}=resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
   console.log(itemCards);
 
   return (
     <div>
       <h1>{name}</h1>
       <h3>{cuisines.join(",")} - {costForTwoMessage} </h3>
-      
+
+      <h2>Menu</h2>
+
       <ul>
-      {itemCards.map(item=><li>{item.card.info.name}</li>)}
+       
+      {itemCards.map(item=><li key={item.card.info.id}>{item.card.info.name} - Rs.{item.card.info.price/100||item.card.info.defaultPrice/100}</li>)}
         
       </ul>
     </div>

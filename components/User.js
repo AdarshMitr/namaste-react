4f3939516bce8099a8ff6e3fb91af40e8a927b4a
AdarshMitr <adarshkumar019@gmail.com>
2023-08-31 12:05:15 +0530

    destructuring props

--- components/User.js
+++ components/User.js
@@ -1,9 +1,11 @@
-const User=(props)=>{
-    return <div className="user-card">
-        <h2>Name: {props.name}</h2>
-        <h3>Location:{props.location} </h3>
-        <h3>Contact:{props.contact}</h3>
+const User = ({ name, location, contact }) => {
+  return (
+    <div className="user-card">
+      <h2>Name: {name}</h2>
+      <h3>Location:{location} </h3>
+      <h3>Contact:{contact}</h3>
     </div>
-}
+  );
+};
 
 export default User;
--- components/UserClass.js
+++ components/UserClass.js
@@ -1,15 +1,24 @@
 import React from "react";
 
 class UserClass extends React.Component{
-    render(){
-        return(
-            <div className="user-card">
-            <h2>Name: {this.props.name}</h2>
-            <h3>Location: {this.props.location} </h3>
-            <h3>Contact:{this.props.contact}</h3>
-        </div>
-        )
+    constructor(props){
+        super(props)
+        console.log(props)  
+
     }
+
+        render(){
+            const[name,location,contact]=this.props;
+            return(
+                <div className="user-card">
+                <h2>Name: {name}</h2>
+                <h3>Location: {location} </h3>
+                <h3>Contact:{contact}</h3>
+            </div>
+            )
+        }
+    
+   
 }
 
 export default UserClass;

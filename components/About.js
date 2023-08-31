56dcfd943300e82a40b975212495dc54ebcdf3e3
AdarshMitr <adarshkumar019@gmail.com>
2023-08-31 17:54:23 +0530

    order of execution of parent and child methods

--- components/About.js
+++ components/About.js
@@ -1,14 +1,37 @@
 import User from "./User";
 import UserClass from "./UserClass";
-const About = (props) => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is Namaste React Web Series</h2>
-      <User name='Adarsh' location='Gurugram' contact='@AdarshMitr'/>
-      <UserClass name='Ashish' location='Rohtak' contact='@AapkaAshish'/>
-    </div>
-  );
-};
+import { Component } from "react";
+
+// Creating it a class based component
+
+class About extends Component{
+
+  constructor(props){
+    super(props);
+    console.log('parent constructor')
+  }
+
+  componentDidMount(){
+    console.log('parent componentDidMount')
+  }
+  render(){
+    console.log('parent render')
+    return (
+    
+      <div>
+        
+        <h1>About</h1>
+        <h2>This is Namaste React Web Series</h2>
+        <User name='Adarsh' location='Gurugram' contact='@AdarshMitr'/>
+        <UserClass name='Ashish' location='Rohtak' contact='@AapkaAshish'/>
+      </div>
+    );
+  }
+}
+
+  
+
+  
+
 
 export default About;
--- components/UserClass.js
+++ components/UserClass.js
@@ -5,15 +5,21 @@ class UserClass extends React.Component {
     super(props);
     console.log(props);
     this.state={
-        count:0
+        count:0,
+        count2:6
     }
+    console.log('child constructor')
   }
 
+  componentDidMount(){
+     console.log('child componentDidMount ')
+  }
   render() {
+    console.log('child render ')
     const {name, location, contact} = this.props;
     return (
       <div className="user-card">
-        <h2>Name: {name}</h2>
+        <h2>Name: {name} (from class)</h2>
         <h3>Location: {location} </h3>
         <h3>Contact:{contact}</h3>
         <h3>Count:{this.state.count}</h3>
@@ -21,6 +27,10 @@ class UserClass extends React.Component {
             //Never update state variable directly 
             this.setState({count:this.state.count+1})
         }}>increment+</button>
+        <h3>Count2:{this.state.count2}</h3>
+        <button onClick={()=>{
+          this.setState({count2:this.state.count2+6})
+        }}>Increment+6</button>
       </div>
     );
   }

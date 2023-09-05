0fb1c26e7f1c9be066afff61dd27ce06a91e2994
AdarshMitr <adarshkumar019@gmail.com>
2023-09-05 15:20:35 +0530

    added ItemList

--- components/RestaurantCategory.js
+++ components/RestaurantCategory.js
@@ -1,13 +1,23 @@
-const RestaurantCategory=({data})=>{
-    console.log(data)
-    return <div>
-        {/* Header */}
+import ItemList from "./ItemList";
 
-       <div className="w-6/12 m-auto bg-gray-50 shadow-lg p-4 mx-auto my-4 flex justify-between"><span className="font-bold text-lg">{data.title} ({data.itemCards.length})</span> 
-        <span  >&#8595;</span>
-       </div>
+const RestaurantCategory = ({ data }) => {
+  //console.log(data);
+  return (
+    <div>
+      {/* Header */}
+
+      <div className="w-6/12 m-auto bg-gray-50 shadow-lg p-4 mx-auto my-4 ">
+        <div className="flex justify-between">
+          <span className="font-bold text-lg">
+            {data.title} ({data.itemCards.length})
+          </span>
+          <span>&#8595;</span>
+        </div>
         {/* Accordion Body */}
+        <ItemList items={data.itemCards} />
+      </div>
     </div>
-}
+  );
+};
 
-export default RestaurantCategory; 
+export default RestaurantCategory;

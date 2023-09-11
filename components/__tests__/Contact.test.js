cd6575360e52cbfcc9fa7e778fdb3da5182bff47
AdarshMitr <adarshkumar019@gmail.com>
2023-09-11 18:23:56 +0530

    some more test-cases in react

--- components/__tests__/Contact.test.js
+++ components/__tests__/Contact.test.js
@@ -5,8 +5,44 @@ import "@testing-library/jest-dom";
 test("Should load 'contact us' component",()=>{
 render(<Contact/>);
 
+//Querying
 const heading=screen.getByRole('heading');
 //Assertion
 expect(heading).toBeInTheDocument();
 
+});
+
+test("Should load 'button' inside component",()=>{
+    render(<Contact/>);
+    
+    //Querying
+
+    // const button=screen.getByRole('button');
+    //more ways to find button
+    //const button= screen.getByText('Submit');
+    const button= screen.getByRole('button');
+    //Assertion
+    expect(button).toBeInTheDocument();
+    
+    });
+
+
+    test("Should load 'input' name inside component",()=>{
+render(<Contact/>);
+
+//Querying
+const inputName=screen.getByPlaceholderText('name');
+//Assertion
+expect(inputName).toBeInTheDocument();
+
+});
+
+test("should load 2 input boxes on the Contact component",()=>{
+  render(<Contact/>);
+  const inputBoxes=screen.getAllByRole('textbox');
+  console.log(inputBoxes);
+  //Assertion
+
+  expect(inputBoxes.length).toBe(2);
+  //expect(inputBoxes.length).not.toBe(3);
 })

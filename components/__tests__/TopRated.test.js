d42a41c6d7ed1407ed8c0338d93c63a47e1abcfb
AdarshMitr <adarshkumar019@gmail.com>
2023-09-13 20:33:01 +0530

    Test for Top Rated Restaurants feature

---
+++ components/__tests__/TopRated.test.js
@@ -0,0 +1,33 @@
+import { fireEvent, render,screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import MOCK_DATA from "../mocks/mockResListData.json"
+import { act } from "react-dom/test-utils";
+import { BrowserRouter } from "react-router-dom";
+import Body from "../Body";
+
+global.fetch=jest.fn(()=>{
+    return Promise.resolve({
+        json:()=>{return Promise.resolve(MOCK_DATA);}
+    })
+})
+
+it('should filter Top Rated Restaurant',async ()=>{
+    await act(async ()=>{
+        render(<BrowserRouter>
+        <Body/>
+        </BrowserRouter>)
+    });
+
+    const cardsBeforeFilter=screen.getAllByTestId('resCard');
+    expect(cardsBeforeFilter.length).toBe(20);
+
+    const topRatedBtn=screen.getByRole("button",{name:"Top Rated Restaurants"});
+
+    fireEvent.click(topRatedBtn);
+
+    const cardsAfterFilter=screen.getAllByTestId('resCard');
+
+    expect(cardsAfterFilter.length).toBe(15);
+    
+
+});
